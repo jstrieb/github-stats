c3e016c5dee3120d35f5ec7ae033c44da293222c
Jacob Strieb <jstrieb@users.noreply.github.com>
2020-08-12 23:07:19 -0400

    Refactor and add comments

--- query.py
+++ query.py
@@ -3,10 +3,19 @@
 import json
 import os
 
+from typing import Dict
+
 import requests
 
 
-def gql_counts_owned():
+###############################################################################
+# GitHub API GraphQL Query Generators
+###############################################################################
+
+def owned_overview_query() -> str:
+    """
+    :return: GitHub GraphQL API query with the star count, fork count, and languages used for the repositories owned by the user
+    """
     return """
 query RepositoryStatsOwned {
   viewer {
@@ -34,7 +43,11 @@ query RepositoryStatsOwned {
   }
 }"""
 
-def gql_counts_contrib():
+
+def contrib_overview_query() -> str:
+    """
+    :return: GitHub GraphQL API query with the star count, fork count, and languages used for the repositories the user has contributed to
+    """
     return """
 query RepositoryStatsContrib {
   viewer {
@@ -62,36 +75,13 @@ query RepositoryStatsContrib {
   }
 }"""
 
-def gql_gazers_contrib(after=""):
-    return f"""
-query StargazersOwned {{
-  viewer {{
-    repositoriesContributedTo(contributionTypes: [COMMIT, PULL_REQUEST, REPOSITORY], first: 100, orderBy: {{field: STARGAZERS, direction: DESC}}) {{
-      nodes {{
-        nameWithOwner
-        stargazers(first: 100{f', after: "{after}"' if after else ""}) {{
-          pageInfo {{
-            hasNextPage
-            endCursor
-          }}
-          nodes {{
-            repositories(isFork: false, first: 10, orderBy: {{field: STARGAZERS, direction: DESC}}) {{
-              nodes {{
-                nameWithOwner
-                stargazers {{
-                  totalCount
-                }}
-              }}
-            }}
-          }}
-        }}
-      }}
-    }}
-  }}
-}}
-"""
 
-def gql_gazers_owned(after=""):
+def owned_stargazers(after: str = "") -> str:
+    """
+    NOTE: Nested f-strings are a little tricky
+    :param after: key used for pagination, if necessary
+    :return: GitHub GraphQL API query with repositories of stargazers on repositories the user owns
+    """
     return f"""
 query StargazersOwned {{
   viewer {{
@@ -120,19 +110,68 @@ query StargazersOwned {{
 }}
 """
 
-def query(query_gen):
+
+def contrib_stargazers(after: str = "") -> str:
+    """
+    NOTE: Nested f-strings are a little tricky
+    :param after: key used for pagination, if necessary
+    :return: GitHub GraphQL API query with repositories of stargazers on repositories the user has contributed to
+    """
+    return f"""
+query StargazersOwned {{
+  viewer {{
+    repositoriesContributedTo(contributionTypes: [COMMIT, PULL_REQUEST, REPOSITORY], first: 100, orderBy: {{field: STARGAZERS, direction: DESC}}) {{
+      nodes {{
+        nameWithOwner
+        stargazers(first: 100{f', after: "{after}"' if after else ""}) {{
+          pageInfo {{
+            hasNextPage
+            endCursor
+          }}
+          nodes {{
+            repositories(isFork: false, first: 10, orderBy: {{field: STARGAZERS, direction: DESC}}) {{
+              nodes {{
+                nameWithOwner
+                stargazers {{
+                  totalCount
+                }}
+              }}
+            }}
+          }}
+        }}
+      }}
+    }}
+  }}
+}}
+"""
+
+
+###############################################################################
+# Helper Functions
+###############################################################################
+
+def query(generated_query: str) -> Dict:
+    """
+    Make a request to the GraphQL API using the authentication token from the
+    environment
+    :param generated_query: string query to be sent to the API
+    :return: decoded GraphQL JSON output
+    """
     access_token = os.getenv("ACCESS_TOKEN")
     headers = {
         "Authorization": f"Bearer {access_token}",
     }
-    generated_query = query_gen()
     r = requests.post("https://api.github.com/graphql", headers=headers,
-            json={"query": generated_query})
+                      json={"query": generated_query})
     return r.json()
 
 
-def main():
-    print(json.dumps(query(gql_counts_owned), indent=2))
+###############################################################################
+# Main Function
+###############################################################################
+
+def main() -> None:
+    print(json.dumps(query(owned_overview_query()), indent=2))
 
 
 if __name__ == "__main__":
