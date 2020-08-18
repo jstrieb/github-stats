d2b0c14a8c5b668cd6929bc2739554ecdfd83784
Jacob Strieb <jstrieb@users.noreply.github.com>
2020-08-18 13:58:28 -0400

    Refactor into classes

--- github_stats.py
+++ github_stats.py
@@ -1,36 +1,60 @@
 #!/usr/bin/python3
 
-import json
 import os
-from builtins import bytes
 
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 import requests
 
 
 ###############################################################################
-# GitHub API GraphQL Query Generators
+# Main Classes
 ###############################################################################
 
-def owned_overview_query(after: str = "") -> str:
-    """
-    :return: GitHub GraphQL API query with the star count, fork count, and languages used for the repositories owned by the user
-    """
-    return f"""
-query RepositoryStatsOwned {{
+class Queries(object):
+    def __init__(self, access_token: str):
+        self.access_token = access_token
+        self.session = requests.Session()
+
+    def query(self, generated_query: str) -> Dict:
+        """
+        Make a request to the GraphQL API using the authentication token from
+        the environment
+        :param generated_query: string query to be sent to the API
+        :return: decoded GraphQL JSON output
+        """
+        headers = {
+            "Authorization": f"Bearer {self.access_token}",
+        }
+        r = self.session.post("https://api.github.com/graphql", headers=headers,
+                              json={"query": generated_query})
+        return r.json()
+
+    @staticmethod
+    def repos_overview(after: Optional[str] = None) -> str:
+        return f"""{{
   viewer {{
-    repositories(ownerAffiliations: OWNER, first: 100, orderBy: {{field: STARGAZERS, direction: DESC}}, isFork: false{f', after: "{after}"' if after else ""}) {{
+    repositoriesContributedTo(
+        first: 100, 
+        includeUserRepositories: true, 
+        orderBy: {{
+            field: UPDATED_AT, 
+            direction: DESC
+        }}, 
+        contributionTypes: COMMIT, 
+        after: {"null" if after is None else '"' + after + '"'}
+    ) {{
+      pageInfo {{
+        hasNextPage
+        endCursor
+      }}
       nodes {{
         nameWithOwner
         stargazers {{
           totalCount
         }}
         forkCount
-        languages(first: 10) {{
-          nodes {{
-            id
-          }}
+        languages(first: 10, orderBy: {{field: SIZE, direction: DESC}}) {{
           edges {{
             size
             node {{
@@ -40,123 +64,14 @@ query RepositoryStatsOwned {{
           }}
         }}
       }}
-      pageInfo {{
-        hasNextPage
-        endCursor
-      }}
-    }}
-  }}
-}}"""
-
-
-def contrib_overview_query() -> str:
-    """
-    :return: GitHub GraphQL API query with the star count, fork count, and languages used for the repositories the user has contributed to
-    """
-    return """
-query RepositoryStatsContrib {
-  viewer {
-    repositoriesContributedTo(contributionTypes: [COMMIT, PULL_REQUEST, REPOSITORY], first: 100, orderBy: {field: STARGAZERS, direction: DESC}) {
-      nodes {
-        nameWithOwner
-        stargazers {
-          totalCount
-        }
-        forkCount
-        languages(first: 10) {
-          nodes {
-            id
-          }
-          edges {
-            size
-            node {
-              name
-              color
-            }
-          }
-        }
-      }
-    }
-  }
-}"""
-
-
-def owned_stargazers(after: str = "") -> str:
-    """
-    NOTE: Nested f-strings are a little tricky
-    :param after: key used for pagination, if necessary
-    :return: GitHub GraphQL API query with repositories of stargazers on repositories the user owns
-    """
-    return f"""
-query StargazersOwned {{
-  viewer {{
-    repositories(ownerAffiliations: OWNER, first: 100, orderBy: {{field: STARGAZERS, direction: DESC}}, isFork: false) {{
-      nodes {{
-        nameWithOwner
-        stargazers(first: 100{f', after: "{after}"' if after else ""}) {{
-          pageInfo {{
-            hasNextPage
-            endCursor
-          }}
-          nodes {{
-            repositories(isFork: false, first: 10, orderBy: {{field: STARGAZERS, direction: DESC}}) {{
-              nodes {{
-                nameWithOwner
-                stargazers {{
-                  totalCount
-                }}
-              }}
-            }}
-          }}
-        }}
-      }}
     }}
   }}
 }}
 """
 
-
-def contrib_stargazers(after: str = "") -> str:
-    """
-    NOTE: Nested f-strings are a little tricky
-    :param after: key used for pagination, if necessary
-    :return: GitHub GraphQL API query with repositories of stargazers on repositories the user has contributed to
-    """
-    return f"""
-query StargazersOwned {{
-  viewer {{
-    repositoriesContributedTo(contributionTypes: [COMMIT, PULL_REQUEST, REPOSITORY], first: 100, orderBy: {{field: STARGAZERS, direction: DESC}}) {{
-      nodes {{
-        nameWithOwner
-        stargazers(first: 100{f', after: "{after}"' if after else ""}) {{
-          pageInfo {{
-            hasNextPage
-            endCursor
-          }}
-          nodes {{
-            repositories(isFork: false, first: 10, orderBy: {{field: STARGAZERS, direction: DESC}}) {{
-              nodes {{
-                nameWithOwner
-                stargazers {{
-                  totalCount
-                }}
-              }}
-            }}
-          }}
-        }}
-      }}
-    }}
-  }}
-}}
-"""
-
-
-def contrib_years() -> str:
-    """
-    TODO
-    :return:
-    """
-    return """
+    @staticmethod
+    def contrib_years() -> str:
+        return """
 query {
   viewer {
     contributionsCollection {
@@ -166,14 +81,9 @@ query {
 }
 """
 
-
-def contribs_by_year(year: str) -> str:
-    """
-    TODO
-    :param year:
-    :return:
-    """
-    return f"""
+    @staticmethod
+    def contribs_by_year(year: str) -> str:
+        return f"""
     year{year}: contributionsCollection(from: "{year}-01-01T00:00:00Z", to: "{year + 1}-01-01T00:00:00Z") {{
       contributionCalendar {{
         totalContributions
@@ -181,15 +91,10 @@ def contribs_by_year(year: str) -> str:
     }}
 """
 
-
-def all_contribs(years: List[str]) -> str:
-    """
-    TODO
-    :param years:
-    :return:
-    """
-    by_years = "\n".join(map(contribs_by_year, years))
-    return f"""
+    @classmethod
+    def all_contribs(cls, years: List[str]) -> str:
+        by_years = "\n".join(map(cls.contribs_by_year, years))
+        return f"""
 query {{
   viewer {{
     {by_years}
@@ -198,82 +103,105 @@ query {{
 """
 
 
-###############################################################################
-# Helper Functions
-###############################################################################
+class Stats(object):
+    def __init__(self, access_token: str):
+        self.queries = Queries(access_token)
 
-def query(generated_query: str) -> Dict:
-    """
-    Make a request to the GraphQL API using the authentication token from the
-    environment
-    :param generated_query: string query to be sent to the API
-    :return: decoded GraphQL JSON output
-    """
-    access_token = os.getenv("ACCESS_TOKEN")
-    headers = {
-        "Authorization": f"Bearer {access_token}",
-    }
-    r = requests.post("https://api.github.com/graphql", headers=headers,
-                      json={"query": generated_query})
-    return r.json()
+        self._stargazers = None
+        self._forks = None
+        self._total_contributions = None
+        self._languages = None
+        self._repos = None
 
+    def __str__(self) -> str:
+        formatted_languages = "\n  - ".join(
+            [f"{l}: {v:0.2f}%" for l, v in self.languages.items()]
+        )
+        return f"""Stargazers: {self.stargazers:,}
+Forks: {self.forks:,}
+Total contributions: {self.total_contributions:,}
+Languages: 
+  - {formatted_languages}"""
 
-def get_stats() -> Dict:
-    """
-    Print summary statistics about forks, stars, and languages
-    """
-    stargazers = 0
-    forks = 0
-    langs = dict()
-    repo_names = set()
+    def get_stats(self):
+        self._stargazers = 0
+        self._forks = 0
+        self._languages = dict()
+        self._repos = set()
 
-    # TODO: Move to separate function
-    after = ""
-    while True:
-        repos = query(owned_overview_query(after)) \
+        next_page = None
+        langs = dict()
+        while True:
+            repos = self.queries.query(Queries.repos_overview(next_page)) \
+                .get("data", {}) \
+                .get("viewer", {}) \
+                .get("repositoriesContributedTo", {})
+
+            for repo in repos.get("nodes", []):
+                self._repos.add(repo.get("nameWithOwner"))
+                self._stargazers += repo.get("stargazers").get("totalCount", 0)
+                self._forks += repo.get("forkCount", 0)
+                for lang in repo.get("languages", {}).get("edges", []):
+                    name = lang.get("node", {}).get("name", "Other")
+                    langs[name] = langs.get(name, 0) + lang.get("size", 0)
+
+            if repos.get("pageInfo", {}).get("hasNextPage", False):
+                next_page = repos.get("pageInfo", {}).get("endCursor")
+            else:
+                break
+
+        # TODO: Improve languages to scale by number of contributions to
+        #       specific filetypes
+        langs_total = sum(langs.values())
+        self._languages = {l: 100 * (s / langs_total) for l, s in langs.items()}
+
+    @property
+    def stargazers(self):
+        if self._stargazers is not None:
+            return self._stargazers
+        self.get_stats()
+        return self._stargazers
+
+    @property
+    def forks(self):
+        if self._forks is not None:
+            return self._forks
+        self.get_stats()
+        return self._forks
+
+    @property
+    def languages(self):
+        if self._languages is not None:
+            return self._languages
+        self.get_stats()
+        return self._languages
+
+    @property
+    def repos(self):
+        if self._repos is not None:
+            return self._repos
+        self.get_stats()
+        return self._repos
+
+    @property
+    def total_contributions(self):
+        if self._total_contributions is not None:
+            return self._total_contributions
+
+        self._total_contributions = 0
+        years = self.queries.query(Queries.contrib_years()) \
             .get("data", {}) \
             .get("viewer", {}) \
-            .get("repositories", {})
-
-        for repo in repos.get("nodes", []):
-            repo_names.add(repo.get("nameWithOwner"))
-            stargazers += repo.get("stargazers").get("totalCount", 0)
-            forks += repo.get("forkCount", 0)
-            for lang in repo.get("languages", {}).get("edges", []):
-                name = lang.get("node", {}).get("name", "Other")
-                langs[name] = langs.get(name, 0) + lang.get("size", 0)
-
-        if repos.get("pageInfo", {}).get("hasNextPage", False):
-            after = repos.get("pageInfo", {}).get("endCursor")
-        else:
-            break
-
-    # TODO: Improve languages to scale by number of contributions to specific
-    #       filetypes
-    langs_total = sum(langs.values())
-    langs_proportional = {l: 100 * (s / langs_total) for l, s in langs.items()}
-
-    total_contribs = 0
-    years = query(contrib_years())\
-        .get("data", {})\
-        .get("viewer", {})\
-        .get("contributionsCollection", {})\
-        .get("contributionYears", [])
-    by_year = query(all_contribs(years))\
-        .get("data", {})\
-        .get("viewer", {}).values()
-    for year in by_year:
-        total_contribs += year\
-            .get("contributionCalendar", {})\
-            .get("totalContributions", 0)
-
-    return {
-        "stargazers": stargazers,
-        "forks": forks,
-        "total_contributions": total_contribs,
-        "owned_repos": len(repo_names),
-        "languages": langs_proportional,
-    }
+            .get("contributionsCollection", {}) \
+            .get("contributionYears", [])
+        by_year = self.queries.query(Queries.all_contribs(years)) \
+            .get("data", {}) \
+            .get("viewer", {}).values()
+        for year in by_year:
+            self._total_contributions += year \
+                .get("contributionCalendar", {}) \
+                .get("totalContributions", 0)
+        return self._total_contributions
 
 
 ###############################################################################
@@ -281,7 +209,9 @@ def get_stats() -> Dict:
 ###############################################################################
 
 def main() -> None:
-    print(json.dumps(get_stats(), indent=2))
+    access_token = os.getenv("ACCESS_TOKEN")
+    s = Stats(access_token)
+    print(s)
 
 
 if __name__ == "__main__":
