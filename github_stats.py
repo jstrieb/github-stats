659343a4671821fc683c3550c9aaf3b49aef3ad2
Erik Westrup <erik.westrup@gmail.com>
2022-02-16 20:08:49 +0100

    Normalize excluded languages comparison
    It's not clear for users how the EXCLUDED_LANGS should be populated:
    * html,tex
    * HTML,TeX
    ?
    
    Avoid all question marks by normalizing both strings to
    lower case before they are compared.
    
    Fixes #64

--- github_stats.py
+++ github_stats.py
@@ -262,7 +262,9 @@ class Stats(object):
         self.username = username
         self._ignore_forked_repos = ignore_forked_repos
         self._exclude_repos = set() if exclude_repos is None else exclude_repos
-        self._exclude_langs = set() if exclude_langs is None else exclude_langs
+        self._exclude_langs = (
+            set() if exclude_langs is None else {x.lower() for x in exclude_langs}
+        )
         self.queries = Queries(username, access_token, session)
 
         self._name: Optional[str] = None
@@ -348,7 +350,7 @@ Languages:
                 for lang in repo.get("languages", {}).get("edges", []):
                     name = lang.get("node", {}).get("name", "Other")
                     languages = await self.languages
-                    if name in self._exclude_langs:
+                    if name.lower() in self._exclude_langs:
                         continue
                     if name in languages:
                         languages[name]["size"] += lang.get("size", 0)
