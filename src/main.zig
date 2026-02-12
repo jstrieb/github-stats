6a2e061b1534de86f0e159a5075cafaf4a89916e
Jacob Strieb <jstrieb@users.noreply.github.com>
2026-02-11 21:14:14 -0500

    Set up logging

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,26 @@
+const builtin = @import("builtin");
 const std = @import("std");
 
+pub const std_options: std.Options = .{
+    .logFn = logFn,
+};
+
+var log_level = std.log.default_level;
+
+fn logFn(
+    comptime message_level: std.log.Level,
+    comptime scope: @TypeOf(.enum_literal),
+    comptime format: []const u8,
+    args: anytype,
+) void {
+    if (@intFromEnum(message_level) <= @intFromEnum(log_level)) {
+        std.log.defaultLog(message_level, scope, format, args);
+    }
+}
+
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    // TODO: Parse environment variables
+    // TODO: Parse CLI flags
+    // TODO: Download statistics to populate data structures
+    // TODO: Output images from templates
 }
