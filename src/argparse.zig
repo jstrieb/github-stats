1520a6cbb7882af5aa031a617d500832a84ffb4d
Jacob Strieb <jstrieb@users.noreply.github.com>
2026-03-01 22:16:07 -0500

    Fix memory leaks

--- src/argparse.zig
+++ src/argparse.zig
@@ -1,5 +1,20 @@
 const std = @import("std");
 
+fn strip_optional(T: type) type {
+    const info = @typeInfo(T);
+    if (info != .optional) return T;
+    return strip_optional(info.optional.child);
+}
+
+fn free_field(allocator: std.mem.Allocator, field: anytype) void {
+    switch (@typeInfo(@TypeOf(field))) {
+        .array => allocator.free(field),
+        .optional => free_field(allocator, field.?),
+        .bool, .int, .float, .@"enum" => {},
+        else => unreachable,
+    }
+}
+
 pub fn parse(allocator: std.mem.Allocator, T: type) !T {
     var arena = std.heap.ArenaAllocator.init(allocator);
     defer arena.deinit();
@@ -8,33 +23,10 @@ pub fn parse(allocator: std.mem.Allocator, T: type) !T {
     const fields = @typeInfo(T).@"struct".fields;
     var seen = [_]bool{false} ** fields.len;
     var result: T = undefined;
-    // TODO: An error when some of the fields are set but not others will
-    // leave dangling pointers
-
-    inline for (fields, &seen) |field, *seen_field| {
-        if (field.defaultValue()) |default| {
-            @field(result, field.name) = default;
-            seen_field.* = true;
-        }
-    }
-
-    {
-        var env = try std.process.getEnvMap(a);
-        defer env.deinit();
-        var iterator = env.iterator();
-        while (iterator.next()) |entry| {
-            const key = try a.dupe(u8, entry.key_ptr.*);
-            defer a.free(key);
-            std.mem.replaceScalar(u8, key, '-', '_');
-            inline for (fields, &seen) |field, *seen_field| {
-                if (std.ascii.eqlIgnoreCase(key, field.name)) {
-                    // TODO: Switch on field type and parse if applicable
-                    @field(result, field.name) = try allocator.dupe(
-                        u8,
-                        entry.value_ptr.*,
-                    );
-                    seen_field.* = true;
-                }
+    errdefer {
+        inline for (fields, seen) |field, seen_field| {
+            if (seen_field) {
+                free_field(allocator, @field(result, field.name));
             }
         }
     }
@@ -42,9 +34,9 @@ pub fn parse(allocator: std.mem.Allocator, T: type) !T {
     {
         const args = try std.process.argsAlloc(a);
         defer std.process.argsFree(a, args);
-        var j: usize = 1;
-        args: while (j < args.len) : (j += 1) {
-            const raw_arg = args[j];
+        var i: usize = 1;
+        args: while (i < args.len) : (i += 1) {
+            const raw_arg = args[i];
             if (std.mem.eql(u8, raw_arg, "-h") or
                 std.mem.eql(u8, raw_arg, "--help"))
             {
@@ -62,11 +54,11 @@ pub fn parse(allocator: std.mem.Allocator, T: type) !T {
             defer a.free(arg);
             std.mem.replaceScalar(u8, arg, '-', '_');
             inline for (fields, &seen) |field, *seen_field| {
-                if (std.ascii.eqlIgnoreCase(arg, field.name)) {
+                if (!seen_field.* and std.ascii.eqlIgnoreCase(arg, field.name)) {
                     // TODO: Switch on field type and parse if applicable
-                    j += 1;
+                    i += 1;
                     // TODO: Fix possible memory leak
-                    @field(result, field.name) = try allocator.dupe(u8, args[j]);
+                    @field(result, field.name) = try allocator.dupe(u8, args[i]);
                     seen_field.* = true;
                     continue :args;
                 }
@@ -75,6 +67,37 @@ pub fn parse(allocator: std.mem.Allocator, T: type) !T {
             std.debug.print("Unknown argument: '{s}'\n", .{raw_arg});
             printUsage(T, args[0]);
             std.process.exit(1);
+        }
+    }
+
+    {
+        var env = try std.process.getEnvMap(a);
+        defer env.deinit();
+        var iterator = env.iterator();
+        while (iterator.next()) |entry| {
+            const key = try a.dupe(u8, entry.key_ptr.*);
+            defer a.free(key);
+            std.mem.replaceScalar(u8, key, '-', '_');
+            inline for (fields, &seen) |field, *seen_field| {
+                if (!seen_field.* and std.ascii.eqlIgnoreCase(key, field.name)) {
+                    // TODO: Switch on field type and parse if applicable
+                    @field(result, field.name) = try allocator.dupe(
+                        u8,
+                        entry.value_ptr.*,
+                    );
+                    seen_field.* = true;
+                }
+            }
+        }
+    }
+
+    inline for (fields, &seen) |field, *seen_field| {
+        if (!seen_field.*) {
+            if (field.defaultValue()) |default| {
+                // TODO: Switch on field type and duplicate if applicable
+                @field(result, field.name) = default;
+                seen_field.* = true;
+            }
         }
     }
 
